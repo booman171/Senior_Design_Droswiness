0923ad6c9ca9fffe9c01c67b2b7bcac29ce91673
unknown <jenario171@yahoo.com>
2019-04-23 16:54:41 -0400

    updated 4_23_19

--- main.py
+++ main.py
@@ -22,6 +22,15 @@ cap=cv2.VideoCapture(0)
 flag=0
 blink = False
 blinkList = []
+
+counter = 0
+inc = 0
+total = 0
+calibrate = True #calibrate, avgLEAR, and avgREAR all need to be initialized at once. Keep them together if we break this out.
+avgLEAR  = 0
+avgREAR = 0
+avgEAR = 0
+blinks = 0
 while True:
     ret, frame=cap.read()
     frame = imutils.resize(frame, width=450)
@@ -34,17 +43,44 @@ while True:
         rightEye = shape[rStart:rEnd]
         leftEAR = eye_aspect_ratio(leftEye)
         rightEAR = eye_aspect_ratio(rightEye)
-        ear = (leftEAR + rightEAR)
-        cv2.putText(frame, ("left eye: %.3f right eye: %.3f flag: %d \n" %(leftEAR, rightEAR, flag)), (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
+        ear = (leftEAR + rightEAR)/2
+        diff = ear - avgEAR
+        thresh = avgEAR*(9/10)
+        
+        if calibrate == True:
+            cv2.putText(frame, ("Calibrating  %.3f" %(leftEAR)), (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        else:
+            #cv2.putText(frame, ("left eye: %.3f" %(leftEAR)), (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            #cv2.putText(frame, ("right eye: %.3f" %(rightEAR)), (10, 50),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            
+            cv2.putText(frame, ("Current EAR: %.3f" %(ear)), (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            cv2.putText(frame, ("Calibrated EAR: %.3f" %(avgEAR)), (10, 50),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            cv2.putText(frame, ("Diff: %.3f" %(abs(avgEAR - ear))), (10, 70),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            cv2.putText(frame, ("flag: %d" %(flag)), (10, 90),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            cv2.putText(frame, ("Blinks: %d" %(total)), (10, 110),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        
         leftEyeHull = cv2.convexHull(leftEye)
         rightEyeHull = cv2.convexHull(rightEye)
         cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
         cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-        
+        if calibrate == True:
+            inc += 1
+            avgLEAR += leftEAR
+            avgREAR += rightEAR
+                #print("EAR: " + str(ear))
+            print("EAR: " + str(ear))
+            if inc == 150:
+                avgLEAR = avgLEAR/inc
+                avgREAR = avgREAR/inc
+                avgEAR = (avgLEAR + avgREAR)/2
+                calibrate = False
+                
+            
         if ((leftEAR < thresh) & (rightEAR < thresh)):
+        #if diff >= 0.1:
             flag += 1
             blink = True
+            counter += 1
             #blinkList.append()
             if flag >= frame_check:
                 cv2.putText(frame, "****************ALERT!****************", (10, 30),
@@ -54,8 +90,11 @@ while True:
 				#print ("Drowsy")
         else:
             flag = 0
-            blink = False
+            if counter >= 2:
+                total += 1
+            counter = 0
     #print ("leftEAR: %.3f rightEAR: %.3f flag: %d \n" %(leftEAR, rightEAR, flag))
+    cv2.namedWindow('Frame',cv2.WINDOW_NORMAL)
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
